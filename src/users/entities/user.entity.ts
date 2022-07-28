1ce11b47564861368318f3dca6bd51c5461417d2
Mason3144 <lts890303@gmail.com>
2022-07-29 00:14:55 +0900

    Hashing password with bcrypt

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -5,8 +5,10 @@ import {
   registerEnumType,
 } from '@nestjs/graphql';
 import { IsEmail, IsString, isString } from 'class-validator';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { InternalServerErrorException } from '@nestjs/common';
 
 enum UserRole {
   Owner,
@@ -32,4 +34,14 @@ export class Users extends CoreEntity {
   @Field((type) => UserRole)
   @Column({ type: 'enum', enum: UserRole })
   role: UserRole;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
+  }
 }
