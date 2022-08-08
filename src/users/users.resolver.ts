96e1c61171f255cbb2425e2eb8dd75fcb2f3c3ac
Mason3144 <lts890303@gmail.com>
2022-08-08 13:27:42 +0900

    Looking up an user profile

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -7,6 +7,7 @@ import {
   CreateAccountOutput,
 } from './dtos/create-account.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
 import { Users } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -46,5 +47,24 @@ export class UsersResolver {
   @UseGuards(AuthGuard)
   me(@AuthUser() authUser: Users) {
     return authUser;
+  }
+
+  @Query((returns) => UserProfileOutput)
+  @UseGuards(AuthGuard)
+  async userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userProfileInput.userId);
+      if (!user) {
+        throw Error();
+      }
+      return {
+        ok: true,
+        user,
+      };
+    } catch (e) {
+      return { error: 'User Not Found', ok: false };
+    }
   }
 }
